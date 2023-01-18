ccbf2e20407966700e7794006868396ffb75e228
Mark <mdevera_@live.com>
2023-01-17 23:38:29 -0500

    edited for mattboi on refresh

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
 
 let coin = document.querySelector(".coin");
-let flipBtn = document.querySelector("#flip-button");
+window.addEventListener("load", (flipCoin));
 
-flipBtn.addEventListener("click", () => {
+function flipCoin(){
     let i = Math.floor(Math.random() * 2);
     coin.style.animation = "none";
     if(i){
         setTimeout(function(){
             coin.style.animation = "spin-heads 3s forwards";
         }, 100);
-        heads++;
     }
     else{
         setTimeout(function(){
             coin.style.animation = "spin-tails 3s forwards";
         }, 100);
-        tails++;
     }
-    setTimeout(updateStats, 3000);
-    disableButton();
-});
+    setTimeout(3000);
+    // disableButton();
+};
 
-function disableButton(){
-    flipBtn.disabled = true;
-    setTimeout(function(){
-        flipBtn.disabled = false;
-    },3000);
-}
-resetBtn.addEventListener("click",() => {
-    coin.style.animation = "none";
-    heads = 0;
-    tails = 0;
-    updateStats();
-});
+// function disableButton(){
+//     flipBtn.disabled = true;
+//     setTimeout(function(){
+//         flipBtn.disabled = false;
+//     },3000);
+// };
